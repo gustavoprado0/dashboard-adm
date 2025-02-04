b95f3d8e7fb74d1789585d8ad86859e78b7d1a37
guuhprado__ <gustavopsz2002@gmail.com>
2025-02-04 13:49:47 -0300

    first commit

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+    return (
+        <main className="w-60 h-screen border-r border-gray-300">
+            <h1 className="m-3">SIDEBAR</h1>
+        </main>
+    )
+}
